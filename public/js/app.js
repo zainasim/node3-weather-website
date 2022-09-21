4888c25f397f3598348aaf2595466565465a5437
Zain Asim <zain.asim@rolustech.net>
2022-09-21 19:17:10 +0500

    Setup app for heruku

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ const messageTwo = document.querySelector('#message-2')
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const location = search.value
-    const url = 'http://localhost:3000/weather?address=' + location
+    const url = '/weather?address=' + location
 
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const weatherDetails = require('./utils/weatherDetail')
 const { read } = require('fs')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 // Define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -88,6 +89,6 @@ app.get('*', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log("server is up on port 3000")
+app.listen(port, () => {
+    console.log("server is up on port " + port)
 }) 
